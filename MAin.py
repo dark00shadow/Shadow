b1d4aeaca9bd83bf7b64608905f991caaa84f4bb
dark00shadow <twistcharlievlogs@gmail.com>
2020-06-16 23:01:53 +0100

    Initial commit

---
+++ MAin.py
@@ -0,0 +1,145 @@
+import pyglet
+from pyglet.window import mouse, key
+from RectangleCollision import *
+from MouseStuff import MouseStateHandler
+
+print('by the way the object limit is 5 and the screen is 600x600')
+Screen_Name = str(input('Screen Name>> '))
+screen_width = int(input('screen width>> '))
+screen_height = int(input('screen height>> '))
+class player():
+    direction = ''
+    posx = float(input('player position x>> '))
+    posy = float(input('player position y>> '))
+    oldposx = posx
+    oldposy = posy
+    image = pyglet.image.load('Textures/'+ str(input('player image>> ')))
+    print('Player created')
+    width = float(input('x size of player>> '))
+    heght = float(input('y size of player>> '))
+class block1():
+    One = str(input('block 1? False/True>> '))
+    if One == 'True':
+        posx1 = float(input('block1 position x>> '))
+        posy1 = float(input('block1 position y>> '))
+        image1 = pyglet.image.load('Textures/'+ str(input('block1 image>> ')))
+        width1 = float(input('x size of block>> '))
+        heght1 = float(input('y size of block>> '))
+        print('block 1 created')
+    Two = str(input('block 2? False/True>> '))
+    if Two == 'True':
+        posx2 = float(input('block2 position x>> '))
+        posy2 = float(input('block2 position y>> '))
+        image2 = pyglet.image.load('Textures/'+ str(input('block2 image>> ')))
+        width2 = float(input('x size of block>> '))
+        heght2 = float(input('y size of block>> '))
+        print('block 2 created')
+    Three = str(input('block 3? False/True>> '))
+    if Three == 'True':
+        posx3 = float(input('block3 position x>> '))
+        posy3 = float(input('block3 position y>> '))
+        image3 = pyglet.image.load('Textures/'+ str(input('block3 image>> ')))
+        width3 = float(input('x size of block>> '))
+        heght3 = float(input('y size of block>> '))
+        print('block 3 created')
+    Four = str(input('block 4? False/True>> '))
+    if Four == 'True':
+        posx4 = float(input('block4 position x>> '))
+        posy4 = float(input('block4 position y>> '))
+        image4 = pyglet.image.load('Textures/'+ str(input('block4 image>> ')))
+        print('block 4 created')
+        width4 = float(input('x size of block>> '))
+        heght4 = float(input('y size of block>> '))
+        print('block 4 created')
+    Five = str(input('block 5? False/True>> '))
+    if Five == 'True':
+        posx5 = float(input('block5 position x>> '))
+        posy5 = float(input('block5 position y>> '))
+        image5 = pyglet.image.load('Textures/'+ str(input('block5 image>> ')))
+        width5 = float(input('x size of block>> '))
+        heght5 = float(input('y size of block>> '))
+        print('block 5 created')
+    def solid(obj1x,obj1y,obj2x,obj2y,obj1w,obj1h,obj2w,obj2h):
+        if collision.rectangle(obj1x,obj1y,obj2x,obj2y,obj1w,obj1h,obj2w,obj2h):
+            if player.direction == 'up': player.posy -= 2
+            if player.direction == 'down': player.posy += 2
+            if player.direction == 'left': player.posx += 2
+            if player.direction == 'right': player.posx -= 2
+class goal():
+    show = False
+    label_text = str(input('text for you won label>> '))
+    label_size = float(input('size of you won label>> '))
+    label_posx = float(input('you won label position x>> '))
+    label_posy = float(input('you won label position y>> '))
+    label = pyglet.text.Label(label_text, x=label_posx,y=label_posy,font_size=label_size)
+    posx = float(input('goal position x>> '))
+    posy = float(input('goal position y>> '))
+    image = pyglet.image.load('Textures/'+ str(input('goal image>> ')))
+    width = float(input('x size of goal>> '))
+    heght = float(input('y size of goal>> '))
+    print('goal created')
+window = pyglet.window.Window(caption=Screen_Name,width=screen_width,height=screen_height)
+key_handler = key.KeyStateHandler()
+mouse_handler = MouseStateHandler()
+window.push_handlers(key_handler)
+window.push_handlers(mouse_handler)
+
+@window.event
+def on_draw():
+    window.clear()
+    if block1.One == 'True':
+        block1.image1.blit(block1.posx1,block1.posy1)
+    if block1.Two == 'True':
+        block1.image2.blit(block1.posx2,block1.posy2)
+    if block1.Three == 'True':
+        block1.image3.blit(block1.posx3,block1.posy3)
+    if block1.Four == 'True':
+        block1.image4.blit(block1.posx4,block1.posy4)
+    if block1.Five == 'True':
+        block1.image5.blit(block1.posx5,block1.posy5)
+    goal.image.blit(goal.posx,goal.posy)
+    if goal.show == True: goal.label.draw()
+    player.image.blit(player.posx,player.posy)
+P_Key_Pressed = False
+U_Key_Pressed = False
+play = False
+def update1(dt):
+    global play, U_Key_Pressed, P_Key_Pressed
+    # Play mode
+    if key_handler[key.P] and P_Key_Pressed == False and play == False:
+        P_Key_Pressed = True
+        play = True
+    if key_handler[key.U] and U_Key_Pressed == False and play == True:
+        P_Key_Pressed = True
+        play = False
+        player.posx = player.oldposx
+        player.posy = player.oldposy
+    if not key_handler[key.P] and P_Key_Pressed == True:
+        P_Key_Pressed = False
+    if not key_handler[key.U] and U_Key_Pressed == True:
+        U_Key_Pressed = False
+    # Collision
+    if collision.rectangle(player.posx,player.posy,goal.posx,goal.posy,player.width,player.heght,goal.width,goal.heght):
+        goal.show = True
+    # Solid
+    if block1.One == 'True': block1.solid(player.posx,player.posy,block1.posx1,block1.posy1,player.heght,player.width,block1.width1,block1.heght1)
+    if block1.Two == 'True': block1.solid(player.posx,player.posy,block1.posx2,block1.posy2,player.heght,player.width,block1.width2,block1.heght2)
+    if block1.Three == 'True': block1.solid(player.posx,player.posy,block1.posx3,block1.posy3,player.heght,player.width,block1.width3,block1.heght3)
+    if block1.Four == 'True': block1.solid(player.posx,player.posy,block1.posx4,block1.posy4,player.heght,player.width,block1.width4,block1.heght4)
+    if block1.Five == 'True': block1.solid(player.posx,player.posy,block1.posx5,block1.posy5,player.heght,player.width,block1.width5,block1.heght5)
+    # Movment
+    if play == True:
+        if key_handler[key.W] and not key_handler[key.A] and not key_handler[key.D] and not key_handler[key.S]:
+            player.posy += 2
+            player.direction = 'up'
+        if not key_handler[key.W] and not key_handler[key.A] and not key_handler[key.D] and key_handler[key.S]:
+            player.posy -= 2
+            player.direction = 'down'
+        if not key_handler[key.W] and key_handler[key.A] and not key_handler[key.D] and not key_handler[key.S]:
+            player.posx -= 2
+            player.direction = 'left'
+        if not key_handler[key.W] and not key_handler[key.A] and key_handler[key.D] and not key_handler[key.S]:
+            player.posx += 2
+            player.direction = 'right'
+pyglet.clock.schedule_interval(update1, 1/120)
+pyglet.app.run()
